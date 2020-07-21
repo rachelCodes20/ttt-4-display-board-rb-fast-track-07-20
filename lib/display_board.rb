9450a531ef79241b4631c0139284f59eabb21459
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 19:28:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,8 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+#Define display_board that accepts a board and prints
+#out the current state.
+
+def display_board(board)
+  board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+  
+ 
+end display_board(board)
