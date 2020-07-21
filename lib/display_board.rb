e45f5b6012a4116fddd380a6cced5e233c37a0c3
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 20:09:37 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,10 +6,16 @@ def display_board(board)
   
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts " --------------------------------------- "
+
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+
+puts " --------------------------------------- "
 
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
+puts " --------------------------------------- "
 end 
+
 board = [" X ","  ","  ","  "," X ","  ","  ","  "," X "]
 display_board(board)
