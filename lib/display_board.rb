f5b721face07a60cd6bf4815837df6104802ec0e
Rachel Collins <rachel.collins@my.liu.edu>
2020-07-21 19:52:55 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,16 +1,3 @@
 #Define display_board that accepts a board and prints
 #out the current state.
 
-def display_board(board)
-  
-  board = ["X", " ", " ", " ", "X", " ", " ", " ", "X"]
-  
-  
-puts "#{board[0]}|#{board[1]}|#{board[3]}"
-puts "-------------"
-puts "#{board[4]}|#{board[5]}|#{board[6]}"
-puts "-------------"
-puts "#{board[7]}|#{board[8]}|#{board[9]}"
- 
-end 
-display_board(board)
